0d576bc06d15846ea2b13a89744525827230cd41
SANDRA1ROY1 <sandraroyaricatt@gmail.com>
2021-11-22 15:20:40 -0500

    initial commit

---
+++ src/main/java/com/example/demojpamysql/DemojpamysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.demojpamysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemojpamysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemojpamysqlApplication.class, args);
+	}
+
+}
